77d6394db601c36a9f5c3414a66689d8e0b2f327
David Blyth <dblyth@anl.gov>
2018-02-07 09:29:29 -0600

    Saving Det1 state

--- src/detectors/Det1.cc
+++ src/detectors/Det1.cc
@@ -2,6 +2,7 @@
 #include <G4LogicalVolume.hh>
 #include <G4NistManager.hh>
 #include <G4PVPlacement.hh>
+#include <G4Tubs.hh>
 
 #include "G4TestBench.hh"
 
@@ -13,9 +14,24 @@ class Det1 : public G4VUserDetectorConstruction {
     G4VPhysicalVolume *Construct() {
         G4NistManager *nist = G4NistManager::Instance();
         G4Material *air = nist->FindOrBuildMaterial("G4_AIR");
+        G4Material *lead = nist->FindOrBuildMaterial("G4_Pb");
 
-        auto worldSolid = new G4Box("worldSolid", 1, 1, 1);
+        auto worldSolid = new G4Box("worldSolid", 10 * CLHEP::m, 10 * CLHEP::m, 10 * CLHEP::m);
         auto worldLog = new G4LogicalVolume(worldSolid, air, "worldLog");
+
+        auto leadBlockSolid = new G4Box("leadBlockSolid", 100 * CLHEP::cm, 100 * CLHEP::cm, 1 * CLHEP::cm);
+        auto leadBlockLog = new G4LogicalVolume(leadBlockSolid, lead, "leadBlockLog");
+        auto blockHoleSolid = new G4Tubs("blockHoleSolid", 0, 0.1 * CLHEP::mm, CLHEP::m, 0, 2 * CLHEP::pi);
+        auto blockHoleLog = new G4LogicalVolume(blockHoleSolid, air, "blockHoleLog");
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                std::stringstream physNameSS;
+                physNameSS << "blockHolePhys(" << i << "," << j << ")";
+                new G4PVPlacement(0, G4ThreeVector(i * 0.25 * CLHEP::mm, (j + 0.5) * 0.25 * CLHEP::mm, 0),
+                                  blockHoleLog, physNameSS.str(), leadBlockLog, true, i * 1000 + j, true);
+            }
+        }
+        new G4PVPlacement(0, G4ThreeVector(), leadBlockLog, "detPhys", worldLog, false, 0, true);
 
         G4VPhysicalVolume *worldPhys =
             new G4PVPlacement(0, G4ThreeVector(), worldLog, "worldPhys", 0, false, 0, true);
