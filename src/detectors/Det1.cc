eea5d66e548dfdc69cc6fbc71f24c71f85415a8b
David Blyth <dblyth@anl.gov>
2018-02-07 13:32:30 -0600

    Saving Det1 state
    Will need to do periodic boundary simulation, looks like, since this configuration takes too much ram.

--- src/detectors/Det1.cc
+++ src/detectors/Det1.cc
@@ -1,4 +1,5 @@
 #include <G4Box.hh>
+#include <G4IntersectionSolid.hh>
 #include <G4LogicalVolume.hh>
 #include <G4NistManager.hh>
 #include <G4PVPlacement.hh>
@@ -19,16 +20,21 @@ class Det1 : public G4VUserDetectorConstruction {
         auto worldSolid = new G4Box("worldSolid", 10 * CLHEP::m, 10 * CLHEP::m, 10 * CLHEP::m);
         auto worldLog = new G4LogicalVolume(worldSolid, air, "worldLog");
 
-        auto leadBlockSolid = new G4Box("leadBlockSolid", 100 * CLHEP::cm, 100 * CLHEP::cm, 1 * CLHEP::cm);
+        auto leadBlockSolid = new G4Box("leadBlockSolid", 5 * CLHEP::m, 5 * CLHEP::m, 5 / 2. * CLHEP::cm);
         auto leadBlockLog = new G4LogicalVolume(leadBlockSolid, lead, "leadBlockLog");
-        auto blockHoleSolid = new G4Tubs("blockHoleSolid", 0, 0.1 * CLHEP::mm, CLHEP::m, 0, 2 * CLHEP::pi);
-        auto blockHoleLog = new G4LogicalVolume(blockHoleSolid, air, "blockHoleLog");
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < 1000; j++) {
+        G4VSolid *blockPoreSolid =
+            new G4Tubs("blockPoreSolid0", 0, 0.005 * CLHEP::mm, 5 * CLHEP::cm, 0, 2 * CLHEP::pi);
+        blockPoreSolid = new G4IntersectionSolid("blockPoreSolid1", leadBlockSolid, blockPoreSolid,
+                                                 new G4RotationMatrix(0, 0.175, 0), G4ThreeVector(0, 0, 0));
+        auto blockPoreLog = new G4LogicalVolume(blockPoreSolid, air, "blockPoreLog");
+        for (int i = -5000; i <= 5000; i++) {
+            for (int j = -5000; j <= 5000; j++) {
                 std::stringstream physNameSS;
-                physNameSS << "blockHolePhys(" << i << "," << j << ")";
-                new G4PVPlacement(0, G4ThreeVector(i * 0.25 * CLHEP::mm, (j + 0.5) * 0.25 * CLHEP::mm, 0),
-                                  blockHoleLog, physNameSS.str(), leadBlockLog, true, i * 1000 + j, true);
+                physNameSS << "blockPorePhys(" << i << "," << j << ")";
+                new G4PVPlacement(0,
+                                  G4ThreeVector((i + 0.5 * j) * 0.25 * CLHEP::mm,
+                                                (0.5 * sqrt(3.) * j) * 0.25 * CLHEP::mm, 0),
+                                  blockPoreLog, physNameSS.str(), leadBlockLog, true, i * 1000 + j, false);
             }
         }
         new G4PVPlacement(0, G4ThreeVector(), leadBlockLog, "detPhys", worldLog, false, 0, true);
